76b163fd9449edeb8245f7ca4a81a2c02eb9e7ab
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-26 15:36:38 +0100

    Refactor

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,54 +1,26 @@
-import CustomerIcon from "../assets/icons/CustomerIcon";
-import HomeIcon from "../assets/icons/HomeIcon";
-import ProjectIcon from "../assets/icons/ProjectsIcon";
-import SalesIcon from "../assets/icons/SalesIcon";
+import { menuItems } from "../helpers/SidebarItems";
 
 export function Sidebar() {
+  const sidebarOptions = menuItems.map((item, index) => (
+    <li key={index}>
+      <a
+        href="#"
+        className="block px-4 py-2 rounded hover:bg-blue-700 hover:text-white flex items-center"
+      >
+        {item.icon}
+        {item.name}
+      </a>
+    </li>
+  ));
+
   return (
     <div className="flex h-screen">
       <aside className="w-64 bg-white text-black flex flex-col">
-        <div className="p-4 text-2xl font-bold">
+        <div className="p-4">
           <img src="/logo.png" alt="Logo" className="h-12 w-auto mx-auto" />
         </div>
         <nav className="flex-1 p-4">
-          <ul className="space-y-2">
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 rounded hover:bg-blue-700 hover:text-white hover:rounded-lg flex items-center"
-              >
-                <HomeIcon className="mr-2 hover:fill-white" />
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 rounded hover:bg-blue-700 hover:text-white flex items-center"
-              >
-                <SalesIcon className="mr-2" />
-                Sales
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 rounded hover:bg-blue-700 hover:text-white flex items-center"
-              >
-                <ProjectIcon className="mr-2" />
-                Projects
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 rounded hover:bg-blue-700 hover:text-white flex items-center"
-              >
-                <CustomerIcon className="mr-2" />
-                Customers
-              </a>
-            </li>
-          </ul>
+          <ul className="space-y-2">{sidebarOptions}</ul>
         </nav>
         <div className="p-4 border-t border-light-gray-700">
           <button className="w-full px-4 py-2 bg-gray-600 hover:bg-gray-500 rounded">
---
+++ frontend/src/helpers/SidebarItems.tsx
@@ -0,0 +1,11 @@
+import HomeIcon from '../assets/icons/HomeIcon';
+import SalesIcon from '../assets/icons/SalesIcon';
+import ProjectIcon from '../assets/icons/ProjectsIcon';
+import CustomerIcon from '../assets/icons/CustomerIcon';
+
+export const menuItems = [
+  { name: "Dashboard", icon: <HomeIcon className="mr-2 hover:fill-white" /> },
+  { name: "Sales", icon: <SalesIcon className="mr-2" /> },
+  { name: "Projects", icon: <ProjectIcon className="mr-2" /> },
+  { name: "Customers", icon: <CustomerIcon className="mr-2" /> },
+];
