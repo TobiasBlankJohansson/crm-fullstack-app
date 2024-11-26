c73857ef4ea91ba92351f562953fa4739b18f695
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-26 17:05:57 +0100

    Add logout in the sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,10 +1,13 @@
+import { UserButton, useUser } from "@clerk/clerk-react";
 import { menuItems } from "../helpers/SidebarItems";
 
 export function Sidebar() {
+  const { user } = useUser();
+
   const sidebarOptions = menuItems.map((item, index) => (
     <li key={index}>
       <a
-        href="#"
+        href={item.href}
         className="block px-4 py-2 rounded hover:bg-blue-700 hover:text-white flex items-center"
       >
         {item.icon}
@@ -22,10 +25,11 @@ export function Sidebar() {
         <nav className="flex-1 p-4">
           <ul className="space-y-2">{sidebarOptions}</ul>
         </nav>
-        <div className="p-4 border-t border-light-gray-700">
-          <button className="w-full px-4 py-2 bg-gray-600 hover:bg-gray-500 rounded">
-            Logout button
-          </button>
+        <div className="ml-4 p-4 border-t border-light-gray-700">
+          <div className="flex items-center">
+            <UserButton />
+            <p className="ml-2">{user?.fullName}</p>
+          </div>
         </div>
       </aside>
     </div>
