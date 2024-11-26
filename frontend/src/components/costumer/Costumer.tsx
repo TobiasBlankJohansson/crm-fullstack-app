a3589d1359673753dfefd841706433fe02dc7cc5
TobiasBlankJohansson <Tobias@annito.se>
2024-11-26 13:46:11 +0100

    feat:add mock for sidebar and nav in costumer
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/components/costumer/Costumer.tsx
+++ frontend/src/components/costumer/Costumer.tsx
@@ -1,11 +1,13 @@
 import { Display } from "./Display";
-import { SideBar } from "./SideBar";
 
 export function Costumer() {
   return (
-    <main>
-      <SideBar/>
-      <Display />
+    <main className="flex">
+      <aside className="h-screen w-1/5 bg-red-500"></aside>
+      <section className="h-screen w-4/5">
+        <nav className="w-full h-10 bg-black"></nav>
+        <Display />
+      </section>
     </main>
   );
 }
--- frontend/src/components/costumer/SideBar.tsx
+++
@@ -1,3 +0,0 @@
-export function SideBar() {
-  return <aside className="h-screen w-1/5 bg-red-500"></aside>;
-}
