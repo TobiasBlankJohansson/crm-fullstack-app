dd6a72b7cd7023bf1bf8525e55e74eb38b2eb237
TobiasBlankJohansson <Tobias@annito.se>
2024-11-26 14:12:55 +0100

    feat:add types for display items
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/components/costumer/DisplayItem.tsx
+++ frontend/src/components/costumer/DisplayItem.tsx
@@ -1,3 +1,14 @@
+type DisplayItem = {
+  title: string;
+  itemProperty: ItemProperty[];
+  Detail: Detail[];
+};
+
+type Detail = {
+  title: string;
+  value: string;
+};
+
 export function DisplayItem() {
-    return <></>
+  return <></>;
 }
