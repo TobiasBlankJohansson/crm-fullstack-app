297d361af33076a95b29739fb5388f0ece64bf23
TobiasBlankJohansson <Tobias@annito.se>
2024-11-27 09:00:54 +0100

    feat:add setDetails for costumerDisplay
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/components/costumer/costumerDisplay.ts
+++ frontend/src/components/costumer/costumerDisplay.ts
@@ -1,15 +1,29 @@
 import { DisplayObject } from "../display/Display";
+import { Detail } from "../display/DisplayItem";
 
 type CostumerDisplayProp = {
   company: string;
   project: string[];
   contact: string[];
-  tag: string;
+  tag: string[];
   adress: string;
   phone: string;
   email: string;
 };
 
-export const costumerDisplay = (fetch:CostumerDisplayProp): DisplayObject[] => {
+export const costumerDisplay = (
+  fetch: CostumerDisplayProp[]
+): DisplayObject[] => {
+  const display: DisplayObject[] = fetch.map((fetch) => {
+    const details = setDetails(fetch);
+    
+  });
   return;
+};
+
+const setDetails = (fetch: CostumerDisplayProp): Detail[] => {
+  const tag: Detail = { title: "Tag", value: fetch.tag.join(",") };
+  const phone: Detail = { title: "Phone", value: fetch.phone };
+  const email: Detail = { title: "Email", value: fetch.email };
+  return [tag, phone, email];
 };
