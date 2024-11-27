688248b306a75482436a1198f16851c3477b9cc6
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-11-27 10:36:11 +0100

    feat: add getmapping in CustomerController
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>

--- backend/src/main/java/salt/takl/crm/controller/CustomerController.java
+++ backend/src/main/java/salt/takl/crm/controller/CustomerController.java
@@ -1,8 +1,13 @@
 package salt.takl.crm.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import salt.takl.crm.model.Customer;
 import salt.takl.crm.service.CustomerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -11,4 +16,11 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @GetMapping
+    public ResponseEntity<List <Customer>> findAll() {
+        List <Customer> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
 }
+
