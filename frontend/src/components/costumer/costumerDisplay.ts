8729da387fd3772b867b23e7491dbb4c8fb0145a
TobiasBlankJohansson <Tobias@annito.se>
2024-11-27 08:43:41 +0100

    feat:add costumerDisplay
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

---
+++ frontend/src/components/costumer/costumerDisplay.ts
@@ -0,0 +1,15 @@
+import { DisplayObject } from "../display/Display";
+
+type CostumerDisplayProp = {
+  company: string;
+  project: string[];
+  contact: string[];
+  tag: string;
+  adress: string;
+  phone: string;
+  email: string;
+};
+
+export const costumerDisplay = (fetch:CostumerDisplayProp): DisplayObject[] => {
+  return;
+};
