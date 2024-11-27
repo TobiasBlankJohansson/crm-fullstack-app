a46a92aaca65aeb732d7a15f002db4335f107900
TobiasBlankJohansson <Tobias@annito.se>
2024-11-27 10:34:37 +0100

    feat:add useEffect to get data from backend and convet it to be displayd
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/components/costumer/Costumer.tsx
+++ frontend/src/components/costumer/Costumer.tsx
@@ -1,52 +1,27 @@
+import { useEffect, useState } from "react";
 import { Sidebar } from "../Sidebar";
 import { Display, DisplayObject } from "../display/Display";
+import { getCostumer } from "@/api/costumer";
+import { costumerDisplay } from "./costumerDisplay";
 
 export function Costumer() {
-  const mockDisplayItems: DisplayObject[] = [
-    {
-      title: "Anchor",
-      count: "1",
-      detail: [
-        { title: "Date", value: "2003" },
-        { title: "Sales", value: "200k" },
-      ],
-      itemProperty: [
-        { title: "Project name", info: ["Anchor"] },
-        {
-          title: "Description",
-          info: [
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, voluptatum?",
-          ],
-        },
-        { title: "Date", info: ["2003"] },
-      ],
-    },
-    {
-      title: "Petpals",
-      count: "2",
-      detail: [
-        { title: "Date", value: "2005" },
-        { title: "Sales", value: "20k" },
-      ],
-      itemProperty: [
-        { title: "Project name", info: ["Petpals"] },
-        {
-          title: "Description",
-          info: [
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, voluptatum?",
-          ],
-        },
-        { title: "Date", info: ["203"] },
-      ],
-    },
-  ];
+  const [costumers, setCostumers] = useState<DisplayObject[]>([]);
+
+  useEffect(() => {
+    const getCostemers = async () => {
+      const fetchData = await getCostumer();
+      const custumer = costumerDisplay(fetchData);
+      setCostumers(() => custumer);
+    };
+    getCostemers(); 
+  }, []);
 
   return (
     <main className="flex h-screen w-screen">
       <Sidebar />
       <section className="h-full w-4/5 flex flex-col">
         <nav className="h-16 bg- border-b-2 border-gray-400"></nav>
-        <Display title="project" displayItems={mockDisplayItems} />
+        <Display title="project" displayItems={costumers} />
       </section>
     </main>
   );
