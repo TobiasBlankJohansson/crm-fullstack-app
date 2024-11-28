5763dc197af0aa107a4ebb47fb72bbf30e5faec8
TobiasBlankJohansson <Tobias@annito.se>
2024-11-28 11:08:52 +0100

    feat:add html in create
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/components/dashbord/create/Create.tsx
+++ frontend/src/components/dashbord/create/Create.tsx
@@ -1,3 +1,13 @@
+
+
 export function Create() {
-  return <></>;
+  return (
+    <section className="pt-20 pb-10 pl-14 w-full h-full">
+      <main className="flex h-full w-full justify-center items-center">
+        <section className="h-full w-3/5 bg-white rounded-2xl">
+          <h2></h2>
+        </section>
+      </main>
+    </section>
+  );
 }
--- frontend/src/components/dashbord/page/customer/Customer.tsx
+++ frontend/src/components/dashbord/page/customer/Customer.tsx
@@ -16,14 +16,13 @@ export function Costumer() {
     };
     getCostemers();
   }, []);
-
+//<Display title="customer" displayItems={costumers} />
   return (
     <main className="flex h-screen w-screen">
       <Sidebar />
       <section className="h-full w-4/5 flex flex-col">
         <nav className="h-16 border-b-2 border-gray-400"></nav>
         <Create/>
-        <Display title="customer" displayItems={costumers} />
       </section>
     </main>
   );
