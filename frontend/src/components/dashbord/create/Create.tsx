60692be74f429c2dbe3be8ec6f1002005fe786a7
TobiasBlankJohansson <Tobias@annito.se>
2024-11-28 11:35:43 +0100

    feat:add form in create
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/components/dashbord/create/Create.tsx
+++ frontend/src/components/dashbord/create/Create.tsx
@@ -1,13 +1,31 @@
+import { Button } from "@/components/ui/button";
+
 type CreateProp = {
   title: string;
+  input: (buttonCreate | inputCreate)[];
 };
 
-export function Create({ title }: CreateProp) {
+type buttonCreate = {
+  label: string;
+  action: () => void;
+};
+
+type inputCreate = {
+  label: string;
+  type: "text" | "email" | "tel";
+};
+
+
+
+export function Create({ title, input}: CreateProp) {
   return (
     <section className="pt-20 pb-10 pl-14 w-full h-full">
       <main className="flex h-full w-full justify-center items-center">
-        <section className="h-full w-3/5 bg-white rounded-2xl">
+        <section className="h-full w-3/5 bg-white rounded-2xl p-5">
           <h2>Create new {title}</h2>
+          <form onSubmit={() => {}}>
+            <Button type="submit">Create</Button>
+          </form>
         </section>
       </main>
     </section>
