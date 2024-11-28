065fc5b638e9a8e3cc169bfcbd86f3d8fee6cdbb
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-28 16:10:34 +0100

    Implement links to buttons and add sections to homepage
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: Lawe02 <lawza02@gmail.com>

--- frontend/src/components/HomePageHeader.tsx
+++ frontend/src/components/HomePageHeader.tsx
@@ -2,18 +2,22 @@ import { SignInButton } from "@clerk/clerk-react";
 
 export function HomePageHeader() {
   return (
-    <header className="flex w-screen shadow-lg justify-between items-center p-4">
+    <header className="sticky top-0 z-50 bg-white flex w-screen shadow-lg justify-between items-center p-4">
       <div>
         <img src="/logo.png" alt="Logo" className="h-12 w-auto" />
       </div>
       <nav className="flex space-x-4">
-        <button className="p-4 py-1 hover:text-blue-700 hover:font-bold inline-block">Home</button>
-        <button className="p-4 py-1 hover:text-blue-700 hover:font-bold inline-block">About</button>
         <button className="p-4 py-1 hover:text-blue-700 hover:font-bold inline-block">
-          Pricing
+          <a href="#home">Home</a>
         </button>
         <button className="p-4 py-1 hover:text-blue-700 hover:font-bold inline-block">
-          Contact
+          <a href="#about">About</a>
+        </button>
+        <button className="p-4 py-1 hover:text-blue-700 hover:font-bold inline-block">
+          <a href="#pricing">Pricing</a>
+        </button>
+        <button className="p-4 py-1 hover:text-blue-700 hover:font-bold inline-block">
+          <a href="#contact">Contact</a>
         </button>
         <div className="p-4 py-1 bg-blue-700 text-white rounded-lg hover:bg-transparent hover:border hover:border-blue-700 hover:text-blue-700 hover:font-bold transition-all duration-300">
           <SignInButton />
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,24 @@
+import AboutSection from "../components/AboutSection";
 import { Carousel } from "../components/Carousel";
+import ContactForm from "../components/ContactForm";
 import { HomePageHeader } from "../components/HomePageHeader";
+import { PricingSection } from "../components/PricingSection";
 
 export function HomePage() {
   return (
     <div>
       <HomePageHeader />
-      <div className="h-screen flex items-center justify-center">
+      <div id="home" className="h-screen flex items-center justify-center">
         <Carousel />
+      </div>
+      <div id="about" className="h-screen flex items-center justify-center bg-white">
+        <AboutSection />
+      </div>
+      <div id="pricing" className="h-screen flex items-center justify-center bg-gray-100">
+        <PricingSection />
+      </div>
+      <div id="contact" className="h-screen flex items-center justify-center bg-blue-50">
+        <ContactForm />
       </div>
     </div>
   );
