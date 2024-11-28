09668b0a7a04a2e806903a0b67b1a5bbf24dea16
TobiasBlankJohansson <Tobias@annito.se>
2024-11-28 16:05:19 +0100

    feat:add logic for costumerCreate
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

---
+++ frontend/src/components/dashbord/page/customer/customerCreate.tsx
@@ -0,0 +1,92 @@
+import { ButtonCreate } from "../../create/ButtonCreate";
+import { CreateObject } from "../../create/Create";
+import { InputCreate } from "../../create/InputCreate";
+
+const id: string[] = [
+  "CostumerCreate-1",
+  "CostumerCreate-2",
+  "CostumerCreate-3",
+  "CostumerCreate-4",
+  "CostumerCreate-5",
+  "CostumerCreate-6",
+  "CostumerCreate-7",
+];
+
+type CreateDto = {
+  company: string;
+  project: string[];
+  contact: contact[];
+  tag: string[];
+  address: string;
+  phone: string;
+  email: string;
+};
+
+type contact = {
+  name: string;
+  phone: string;
+  email: string;
+};
+
+export function costumerCreate(): CreateObject {
+  const input: JSX.Element[] = inputs();
+  return { input, onSubmit };
+}
+
+function inputs() {
+  const company = <InputCreate id={id[0]} label="Company" type="text" />;
+  const project = (
+    <ButtonCreate
+      id={id[1]}
+      label={"Project"}
+      click={function (
+        setObject: React.Dispatch<React.SetStateAction<object[]>>
+      ): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
+  const contact = (
+    <ButtonCreate
+      id={id[2]}
+      label={"Project"}
+      click={function (
+        setObject: React.Dispatch<React.SetStateAction<object[]>>
+      ): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
+  const tag = (
+    <ButtonCreate
+      id={id[3]}
+      label={"Project"}
+      click={function (
+        setObject: React.Dispatch<React.SetStateAction<object[]>>
+      ): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
+
+  const address = <InputCreate id={id[4]} label="Address" type="text" />;
+  const phone = <InputCreate id={id[5]} label="Phone" type="tel" />;
+  const email = <InputCreate id={id[6]} label="Email" type="email" />;
+
+  return [company, project, contact, tag, address, phone, email];
+}
+
+function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  const list = id.map((id) => document.getElementById(id).value);
+  const save: CreateDto = {
+    company: list[0],
+    project: list[1],
+    contact: list[2],
+    tag: list[3],
+    address: list[4],
+    phone: list[5],
+    email: list[6],
+  };
+  console.log(save);
+}
