5c8623fb8e134f6d10ddfec117ca7f5d5c5c6e7e
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-11-29 13:14:02 +0100

    refa: refactor customerController to use customerMapper Co-authored-by: Lawe02 <lawza02@gmail.com> Co-authored-by: TobiasBlankJohansson <tobias@annito.se> Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com> Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- backend/src/main/java/salt/takl/crm/controller/CustomerController.java
+++ backend/src/main/java/salt/takl/crm/controller/CustomerController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import salt.takl.crm.dto.response.ContactResponseDTO;
 import salt.takl.crm.dto.response.CustomerResponseDTO;
+import salt.takl.crm.mappers.CustomerMapper;
 import salt.takl.crm.model.Customer;
-import salt.takl.crm.model.Project;
+
 import salt.takl.crm.service.CustomerService;
 
 import java.util.List;
@@ -17,8 +17,12 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+
     private final CustomerService customerService;
-    public CustomerController(CustomerService customerService) {
+    private final CustomerMapper customerMapper;
+
+    public CustomerController(CustomerService customerService, CustomerMapper customerMapper) {
+        this.customerMapper = customerMapper;
         this.customerService = customerService;
     }
 
@@ -26,48 +30,15 @@ public class CustomerController {
     public ResponseEntity<List <CustomerResponseDTO>> findAll() {
         List <Customer> customers = customerService.getAllCustomers();
         List <CustomerResponseDTO> customerResponseDTOS = customers.stream()
-                .map(customer -> new CustomerResponseDTO(
-                        customer.getId(),
-                        customer.getCompanyName(),
-                        customer.getAddress(),
-                        customer.getPhoneNumber(),
-                        customer.getEmail(),
-                        customer.getProjects().stream().map(Project::getName).toList(),
-                        customer.getContacts().stream().map(contact -> new ContactResponseDTO(
-                                contact.getId(),
-                                contact.getFirstName(),
-                                contact.getPhone(),
-                                contact.getEmail())).toList(),
-                        customer.getTags().stream().map(t -> t.name()).toList()
-                )).toList();
+                .map(customer -> customerMapper.customerToResponseDTO(customer)
+                ).toList();
         return ResponseEntity.ok(customerResponseDTOS);
     }
 
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerResponseDTO> findById(@PathVariable String customerId) {
         Customer customer = customerService.getCustomerById(UUID.fromString(customerId));
-        List<ContactResponseDTO> contacts = customer.getContacts()
-                .stream()
-                .map(contact -> new ContactResponseDTO(
-                        contact.getId(),
-                        contact.getFirstName(),
-                        contact.getPhone(),
-                        contact.getEmail())
-                ).toList();
-        List<String> projects = customer.getProjects()
-                .stream()
-                .map(Project::getName)
-                .toList();
-        CustomerResponseDTO result = new CustomerResponseDTO(
-                customer.getId(),
-                customer.getCompanyName(),
-                customer.getAddress(),
-                customer.getPhoneNumber(),
-                customer.getEmail(),
-                projects,
-                contacts,
-                customer.getTags().stream().map(t -> t.name()).toList()
-        );
+        CustomerResponseDTO result = customerMapper.customerToResponseDTO(customer);
         return ResponseEntity.ok(result);
     }
 }
