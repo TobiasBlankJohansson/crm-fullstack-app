415b03afbcc98665dc540c4740bce7e39a4f1613
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-11-30 19:08:48 +0100

    feat: add CustomerRequestDTO
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>

--- backend/src/main/java/salt/takl/crm/dto/request/CustomerRequestDTO.java
+++ backend/src/main/java/salt/takl/crm/dto/request/CustomerRequestDTO.java
@@ -1,4 +1,8 @@
 package salt.takl.crm.dto.request;
 
-public record CustomerRequestDTO (){
-}
+public record CustomerRequestDTO(
+        String companyName,
+        String address,
+        String phoneNumber,
+        String email
+) {}
