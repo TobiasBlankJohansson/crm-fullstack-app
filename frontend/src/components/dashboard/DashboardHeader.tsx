7682d5b9f15cebbdecf8f499bf93e3aca092734a
TobiasBlankJohansson <tobias@annito.se>
2024-12-01 20:50:41 +0100

    feat:add navigate to add new button

--- frontend/src/components/dashboard/DashboardHeader.tsx
+++ frontend/src/components/dashboard/DashboardHeader.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import AddIcon from "../../assets/icons/AddIcon";
 
 type DashboardHeaderProp = {
@@ -5,6 +6,8 @@ type DashboardHeaderProp = {
 };
 
 export function DashboardHeader({ path }: DashboardHeaderProp) {
+  const navigate = useNavigate();
+
   return (
     <header className="h-16 border-b-2 border-gray-400 flex justify-end pr-10 shadow-sm">
       <div className="flex items-center gap-4 ml-auto">
@@ -14,7 +17,10 @@ export function DashboardHeader({ path }: DashboardHeaderProp) {
           className="px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
 
-        <button className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 text-black rounded-full hover:bg-gray-100">
+        <button
+          className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-300 text-black rounded-full hover:bg-stone-100"
+          onClick={() => navigate("/" + path + "/create")}
+        >
           Add new
           <AddIcon className="w-5 h-5" />
         </button>
