568358317bb2f2f1dcba29ddef20444e8a5372fb
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-12-02 21:26:13 +0100

    feat: add footer to HomePage
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,23 +2,39 @@ import AboutSection from "../components/homepage/AboutSection";
 import { Carousel } from "../components/homepage/Carousel";
 import ContactForm from "../components/homepage/ContactForm";
 import { PricingSection } from "../components/homepage/PricingSection";
+import { Footer } from "@/components/homepage/Footer";
 
 export function HomePage() {
   return (
     <div>
-      <div id="home" className="h-screen flex flex-col items-center justify-start px-4 pb-10 pt-10">
-        <h1 className="text-3xl font-bold text-blue-700 mt-10">Manage, Organize, and Build: Unlock the Power of Our CRM</h1>
+      <div
+        id="home"
+        className="h-screen flex flex-col items-center justify-start px-4 pb-10 pt-10"
+      >
+        <h1 className="text-3xl font-bold text-blue-700 mt-10">
+          Manage, Organize, and Build: Unlock the Power of Our CRM
+        </h1>
         <Carousel />
       </div>
-      <div id="about" className="h-screen flex items-center justify-center bg-white">
+      <div
+        id="about"
+        className="h-screen flex items-center justify-center bg-white"
+      >
         <AboutSection />
       </div>
-      <div id="pricing" className="h-screen flex items-center justify-center px-8">
+      <div
+        id="pricing"
+        className="h-screen flex items-center justify-center px-8"
+      >
         <PricingSection />
       </div>
-      <div id="contact" className="h-screen flex items-center justify-center bg-blue-50">
+      <div
+        id="contact"
+        className="h-screen flex items-center justify-center bg-blue-50"
+      >
         <ContactForm />
       </div>
+      <Footer />
     </div>
   );
 }
