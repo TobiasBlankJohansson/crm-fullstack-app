409408f8f9b2be84767e62b150f2e8e2310f5ba8
TobiasBlankJohansson <Tobias@annito.se>
2024-12-03 14:14:16 +0100

    feat:add saleToDto to convert to responseDto
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- backend/src/main/java/salt/takl/crm/controller/Sales/SalesResponseDTO.java
+++ backend/src/main/java/salt/takl/crm/controller/Sales/SalesResponseDTO.java
@@ -1,8 +1,18 @@
 package salt.takl.crm.controller.Sales;
 
+import salt.takl.crm.model.Sale;
+
 public record SalesResponseDTO (
         String name,
         String customer,
         String project,
         String sales
-){}
+){
+    public static SalesResponseDTO saleToDto(Sale sale) {
+        return new SalesResponseDTO(
+                sale.name(),
+                sale.getCustomer().getCompanyName(),
+                sale.getProject().getName(),
+                sale.getSalesAmount()+"");
+    }
+}
