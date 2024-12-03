045989167f3cf140fcac1725cab58a9a7ab37a16
KatheRojasO <kathe.rojaso@gmail.com>
2024-12-03 18:01:20 +0100

    Create endpoint to update a project

--- backend/src/main/java/salt/takl/crm/controller/ProjectController.java
+++ backend/src/main/java/salt/takl/crm/controller/ProjectController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import salt.takl.crm.dto.request.ProjectRequestDTO;
 import salt.takl.crm.dto.response.ProjectResponseDTO;
+import salt.takl.crm.model.Project;
+import salt.takl.crm.repository.ProjectRepository;
 import salt.takl.crm.service.ProjectService;
 
 import java.util.List;
@@ -16,13 +18,9 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
+
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
-    }
-
-    @GetMapping
-    public List<ProjectResponseDTO> getAllProjects() {
-        return projectService.getAllProjects();
     }
 
     @PostMapping
@@ -31,9 +29,26 @@ public class ProjectController {
         return new ResponseEntity<>(createdProject, HttpStatus.CREATED);
     }
 
+    @GetMapping
+    public List<ProjectResponseDTO> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+
     @GetMapping("/{projectId}")
     public ResponseEntity<ProjectResponseDTO> findProjectById(@PathVariable String projectId) {
         ProjectResponseDTO project = projectService.getProjectById(UUID.fromString(projectId));
         return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    @PutMapping("/{projectId}")
+    public ResponseEntity<ProjectResponseDTO> updateProject(@PathVariable String projectId, @RequestBody ProjectRequestDTO projectRequestDTO) {
+        ProjectResponseDTO updatedProject = projectService.updateProject(UUID.fromString(projectId), projectRequestDTO);
+        return new ResponseEntity<>(updatedProject, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<ProjectResponseDTO> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProject(UUID.fromString(projectId));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
