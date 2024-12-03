a5ec65911e2145c40a7c07b0722720e3dcefffab
TobiasBlankJohansson <Tobias@annito.se>
2024-12-03 12:05:09 +0100

    feat:add createSales with mock return
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- backend/src/main/java/salt/takl/crm/controller/Sales/SalesController.java
+++ backend/src/main/java/salt/takl/crm/controller/Sales/SalesController.java
@@ -1,7 +1,9 @@
 package salt.takl.crm.controller.Sales;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import salt.takl.crm.service.CustomerService;
@@ -32,6 +34,9 @@ public class SalesController {
         return ResponseEntity.ok(sales);
     }
 
-
+    @PostMapping("/sales")
+    public ResponseEntity<SalesResponseDTO> createSales(){
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
 
 }
