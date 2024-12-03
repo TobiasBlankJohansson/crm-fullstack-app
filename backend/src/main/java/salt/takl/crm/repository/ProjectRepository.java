f29cae6fd65962aa9bd4f339e98f5fda9812052b
KatheRojasO <kathe.rojaso@gmail.com>
2024-12-03 18:00:46 +0100

    Update project repository to make it find projects by id

--- backend/src/main/java/salt/takl/crm/repository/ProjectRepository.java
+++ backend/src/main/java/salt/takl/crm/repository/ProjectRepository.java
@@ -3,13 +3,18 @@ package salt.takl.crm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import salt.takl.crm.model.Project;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
+    Optional<Project> findProjectById(UUID id);
+
     @Query("SELECT p FROM Project p WHERE p.started <= :currentDate AND p.ended >= :currentDate")
     List<Project> findActiveProjects(@Param("currentDate") LocalDateTime currentDate);
 
