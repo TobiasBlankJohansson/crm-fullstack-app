964e3c29eaa0fbf358e7d971791ec2537539a09d
KatheRojasO <kathe.rojaso@gmail.com>
2024-12-03 12:14:49 +0100

    Add method that updated a customer

--- backend/src/main/java/salt/takl/crm/service/CustomerService.java
+++ backend/src/main/java/salt/takl/crm/service/CustomerService.java
@@ -9,7 +9,8 @@ import java.util.UUID;
 
 @Service
 public class CustomerService {
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
+
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
@@ -22,8 +23,15 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
-    public Customer updateCustomer(Customer customer) {
-        return customerRepository.save(customer);
+    public Customer updateCustomer(UUID customerId, Customer updatedCustomer) {
+        Customer existingCustomer = customerRepository.findCustomerById(customerId);
+
+        existingCustomer.setPhoneNumber(updatedCustomer.getPhoneNumber());
+        existingCustomer.setEmail(updatedCustomer.getEmail());
+        existingCustomer.setAddress(updatedCustomer.getAddress());
+        existingCustomer.setCompanyName(updatedCustomer.getCompanyName());
+
+        return customerRepository.save(existingCustomer);
     }
 
     public void deleteCustomer(Customer customer) {
@@ -31,7 +39,7 @@ public class CustomerService {
     }
 
     public Customer getCustomerById(UUID id) {
-        return customerRepository.getCustomersById(id);
+        return customerRepository.findCustomerById(id);
     }
 
     public Customer saveCustomer(Customer customer) {
