f4953c94ab10da3d71ee1d2bf325b0869504f544
TobiasBlankJohansson <Tobias@annito.se>
2024-12-04 13:30:06 +0100

    feat:add deleteSale in api
    Co-authored-by: TobiasBlankJohansson <tobias@annito.se>
    Co-authored-by: KatheRojasO <kathe.rojaso@gmail.com>
    Co-authored-by: Lawe02 <lawza02@gmail.com>
    Co-authored-by: Alexander-Gabor <gaboralexandergabor@gmail.com>

--- frontend/src/api/sales.ts
+++ frontend/src/api/sales.ts
@@ -73,66 +73,11 @@ export const updateSale = async (
   }
 };
 
-
-// const mockSales = [
-//   {
-//     name: "Sold AI Development Services",
-//     company: "Tech Innovators Inc.",
-//     project: "AI Development",
-//     sale: "$150,000",
-//   },
-//   {
-//     name: "Completed Solar Panel Deployment",
-//     company: "Green Energy Solutions",
-//     project: "Solar Panel Deployment",
-//     sale: "$250,000",
-//   },
-//   {
-//     name: "Provided Automated Assembly Line",
-//     company: "NextGen Robotics",
-//     project: "Automated Assembly Line",
-//     sale: "$400,000",
-//   },
-//   {
-//     name: "Developed Home Security App",
-//     company: "Smart Home Systems",
-//     project: "Home Security App",
-//     sale: "$120,000",
-//   },
-//   {
-//     name: "Integrated Blockchain Technology",
-//     company: "FutureTech Labs",
-//     project: "Blockchain Integration",
-//     sale: "$300,000",
-//   },
-//   {
-//     name: "Deployed Electric Vehicle Chargers",
-//     company: "EcoTech Solutions",
-//     project: "Electric Vehicle Charger",
-//     sale: "$200,000",
-//   },
-//   {
-//     name: "Upgraded Cloud Infrastructure",
-//     company: "CloudCom LLC",
-//     project: "Cloud Infrastructure Upgrade",
-//     sale: "$500,000",
-//   },
-//   {
-//     name: "Built E-Learning Platform",
-//     company: "EduTech Partners",
-//     project: "E-Learning Platform",
-//     sale: "$350,000",
-//   },
-//   {
-//     name: "Created Telemedicine Application",
-//     company: "HealthTech Solutions",
-//     project: "Telemedicine App",
-//     sale: "$220,000",
-//   },
-//   {
-//     name: "Installed Smart Irrigation System",
-//     company: "AgriTech Innovations",
-//     project: "Smart Irrigation System",
-//     sale: "$180,000",
-//   },
-// ];
+export const deleteSale = async (saleId: UUID): Promise<boolean> => {
+  const response = await axios.delete(`${path}/api/sales/${saleId}`);
+  if (response.status >= 200 && response.status < 300) {
+    return true;
+  } else {
+    throw new Error(`Unexpected response status: ${response.status}`);
+  }
+};
